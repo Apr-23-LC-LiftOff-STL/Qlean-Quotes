eebd15c349c796a10f62251b0e6ab47285667551
“Bethany <Bethanygellington@gmail.com>
2023-05-06 15:32:23 +0800

    reconfigures CustomerController and to handle login and register forms, previously AuthenticationController handled it

--- src/main/java/org/launchcode/qleanquotes/controllers/CustomerController.java
+++ src/main/java/org/launchcode/qleanquotes/controllers/CustomerController.java
@@ -1,7 +1,19 @@
 package org.launchcode.qleanquotes.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.launchcode.qleanquotes.models.Customer;
+import org.launchcode.qleanquotes.models.data.CustomerRepository;
+import org.launchcode.qleanquotes.models.dto.LoginFormDTO;
+import org.launchcode.qleanquotes.models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -18,40 +30,77 @@ cross-origin requests from a specified origin.*/
 @Controller
 public class CustomerController {
 
-//    @Autowired
-//    private CustomerRepository customerRepository;
-//
-//    @GetMapping("")
-//    public Iterable<Customer>  getCustomer(){
-//        return customerRepository.findAll();
-//    }
-//
-//
-//    @PostMapping("/register")
-//    void addCustomer(@RequestBody Customer customer){
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        Customer addCustomer = new Customer(customer.getName(), customer.getLastName(), customer.getEmail(),
-//                encoder.encode(customer.getPwHash()), customer.getPhoneNumber(), customer.getAddress());
-//        customerRepository.save(addCustomer);
-//    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
-//    @PostMapping("authenticate")
-//    public boolean customerAuthenticate(@RequestBody Customer customer) {
-//
-//        Optional<Customer> customerData = customerRepository.findById(customer.getId());
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//        if(customerData.isPresent()) {
-//            Customer customerInfo = customerData.get();
-//            if(encoder.matches(customer.getPwHash(), customerInfo.getPwHash())){
-//                return true;
-//            } else {
-//                return false;
-//            }
-//        }else{
-//            return false;
-//        }
-//    }
+    @GetMapping("/register")
+    public String displayRegisterForm(Model model){
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+
+    @PostMapping("/register")
+    public  String processRegisterForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO, Errors errors,
+                                       HttpServletRequest request){
+        if(errors.hasErrors()){
+            return "/register";
+        }
+        Customer existingCustomer = customerRepository.findByEmail(registerFormDTO.getEmail());
+
+        if(existingCustomer != null){
+            errors.rejectValue("email", "email.alreadyexists", "A user with that email already exists");
+            return "/login";
+        }
+
+        // Send customer back to form if passwords didn't match
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if(!password.equals(verifyPassword)){
+            errors.rejectValue("password" , "passwords.mismatch", "Passwords do not match");
+            return "/register";
+        }
+
+        // OTHERWISE, save new email , hashed password and other info in database, and redirect to home page
+        Customer newCustomer = new Customer(registerFormDTO.getName(), registerFormDTO.getLastName(),registerFormDTO.getEmail(),registerFormDTO.getPassword());
+        customerRepository.save(newCustomer);
+        return "redirect:/landing";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model){
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO, Errors errors,
+                                   HttpServletRequest request){
+
+        if(errors.hasErrors()){
+            return "/login";
+        }
+
+        // Look up user in database using email they provided in the form
+        Customer theCustomer = customerRepository.findByEmail(loginFormDTO.getEmail());
+
+
+        if(theCustomer == null){
+            errors.rejectValue("email", "customer.invalid", "The given email does not exist");
+            return "/login";
+        }
+
+        String password = loginFormDTO.getPassword();
+
+        if(!theCustomer.isMatchingPassword(password)){
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+            return "/login";
+        }
+
+        return  "redirect:/landing";
+    }
 
 
 
