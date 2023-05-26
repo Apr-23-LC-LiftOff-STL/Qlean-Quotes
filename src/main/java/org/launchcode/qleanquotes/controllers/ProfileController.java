a9d8d0a8400beeab62789ccaea08884d0fccc910
“Bethany <Bethanygellington@gmail.com>
2023-05-26 09:38:13 +0800

    adds phone number field to customer, adds profileformdto

--- src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
+++ src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
@@ -2,6 +2,8 @@ package org.launchcode.qleanquotes.controllers;
 
 
 import org.launchcode.qleanquotes.models.Customer;
+import org.launchcode.qleanquotes.models.dto.ProfileFormDTO;
+import org.launchcode.qleanquotes.models.dto.RegisterFormDTO;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
@@ -15,11 +17,14 @@ public class ProfileController {
         Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("customer", customer);
         model.addAttribute("customerName", customer.getName());
+        model.addAttribute(new ProfileFormDTO());
         model.addAttribute("title", "profile");
         return "profile";
     }
 
 }
+
+
 
 
 
