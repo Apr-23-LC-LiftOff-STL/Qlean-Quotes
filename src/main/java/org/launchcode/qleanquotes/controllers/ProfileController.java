0fffcfe0a8594169bc08641026909a8aaae9d399
RebeccaMines <becca.mines22297@gmail.com>
2023-05-30 21:16:43 -0500

    Phone number is saving to database and is visible sometimes

--- src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
+++ src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
@@ -1,10 +1,14 @@
 package org.launchcode.qleanquotes.controllers;
 
 
+import jakarta.validation.Valid;
 import org.launchcode.qleanquotes.models.Customer;
+import org.launchcode.qleanquotes.models.data.CustomerRepository;
 import org.launchcode.qleanquotes.models.dto.ProfileFormDTO;
 import org.launchcode.qleanquotes.models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
-
+@Autowired
+private CustomerRepository customerRepository;
     @GetMapping("/profile")
     public String displayProfilePage(Model model) {
         Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -26,8 +31,16 @@ public class ProfileController {
 
 
     @PostMapping("/profile")
-    public String savePhoneNumber(@ModelAttribute("phoneNumber") ProfileFormDTO phoneNumberForm, Model model) {
+    public String savePhoneNumber(@ModelAttribute @Valid ProfileFormDTO phoneNumberForm, Model model, Errors errors) {
+        Customer existingCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("phoneNumber", phoneNumberForm.getPhoneNumber());
+        existingCustomer.setPhoneNumber(phoneNumberForm.getPhoneNumber());
+        customerRepository.save(existingCustomer);
+
+        if (errors.hasErrors()) {
+            return "/profile";
+        }
+
         return "redirect:/updated-profile";
     }
 
