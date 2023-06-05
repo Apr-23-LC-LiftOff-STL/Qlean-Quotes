48e10ef85e4907f462da5743fb64175daf49acfb
“Bethany <Bethanygellington@gmail.com>
2023-06-05 16:36:19 +0800

    fixes phone number formatting

--- src/main/java/org/launchcode/qleanquotes/models/dto/ProfileFormDTO.java
+++ src/main/java/org/launchcode/qleanquotes/models/dto/ProfileFormDTO.java
@@ -7,7 +7,8 @@ import jakarta.validation.constraints.Size;
 
 public class ProfileFormDTO {
 
-    @Size(min = 10, max = 11, message="Phone number must be at least 10 digits, without dashes")
+    @Pattern(regexp = "^(?:\\d{3}-?){1,2}\\d{4}$", message = "Phone number must be numeric and may include dashes, Ex: 123-123-1234")
+    @Size(min = 10, max = 12, message="Phone number must be at least 10 digits and may not exceed 11")
     private String phoneNumber;
 
     public String getPhoneNumber() {
