824f6874291982a797ae3df5692b3154f206858e
Rainy <112306653+rainy-moss@users.noreply.github.com>
2023-06-05 08:51:17 -0500

    quote data persists throughout all pages

--- src/main/java/org/launchcode/qleanquotes/controllers/NewMoneyController.java
+++ src/main/java/org/launchcode/qleanquotes/controllers/NewMoneyController.java
@@ -11,7 +11,6 @@ import org.launchcode.qleanquotes.wrappers.CachedBodyHttpServletRequest;
 import org.launchcode.qleanquotes.wrappers.SquareWrapper;
 import org.launchcode.qleanquotes.models.Customer;
 import org.launchcode.qleanquotes.models.PaymentResult;
-import org.launchcode.qleanquotes.models.dto.CreateQuoteFormDTO;
 import org.launchcode.qleanquotes.models.dto.PaymentFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,39 +21,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-import static org.launchcode.qleanquotes.controllers.QuoteController.getQuoteIdFromSession;
+import static org.launchcode.qleanquotes.controllers.QuoteController.quoteSessionKey;
 
 @Controller
     public class NewMoneyController {
-        private final SquareWrapper squareWrapper;
-        @Autowired
-        private QuoteRepository quoteRepository;
 
-        @Autowired
-        public NewMoneyController(SquareWrapper squareWrapper) {
-            this.squareWrapper = squareWrapper;
-        }
+    private final SquareWrapper squareWrapper;
 
-        @GetMapping("/payment/{quoteid}")
-        public String showPaymentForm(@ModelAttribute CreateQuoteFormDTO createQuoteFormDTO,  @PathVariable int quoteId, CachedBodyHttpServletRequest request, Model model){
+    @Autowired
+    public NewMoneyController(SquareWrapper squareWrapper) {
+        this.squareWrapper = squareWrapper;
+    }
+
+        @GetMapping("/payment")
+        public String showPaymentForm(HttpSession session, Model model) {
             Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             model.addAttribute("customer", customer);
-            HttpSession session = request.getSession();
-            Integer storedQuoteId = getQuoteIdFromSession(session);
-            Optional<Quote> optionalQuote = quoteRepository.findById(quoteId);
-
-            if (storedQuoteId != null && storedQuoteId == quoteId) {
-                Quote quote = optionalQuote.get();
+            Quote quote = (Quote) session.getAttribute(quoteSessionKey);
+            if (quote != null) {
                 model.addAttribute("quote", quote);
             }
-            model.addAttribute(new PaymentFormDTO());
+            model.addAttribute("paymentFormDTO", new PaymentFormDTO());
             return "payment";
         }
-
         @RequestMapping("")
         public String index(Map<String, Object> model) throws InterruptedException, ExecutionException {
         model.put("paymentFormUrl",
@@ -66,72 +58,55 @@ import static org.launchcode.qleanquotes.controllers.QuoteController.getQuoteIdF
         return "payment";
     }
 
-
-    @PostMapping("/payment/{quoteId}")
-    public String createPaymentRequest (@ModelAttribute PaymentFormDTO paymentFormDTO, @PathVariable int quoteId, CachedBodyHttpServletRequest request, Model model, Errors errors) {
+    @PostMapping("/payment")
+    public String createPaymentRequest(@ModelAttribute PaymentFormDTO paymentFormDTO,
+                                       HttpSession session, Model model) {
         Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("customer", customer);
-        HttpSession session = request.getSession();
-        Integer storedQuoteId = getQuoteIdFromSession(session);
-        Optional<Quote> optionalQuote = quoteRepository.findById(quoteId);
-        if (storedQuoteId != null && storedQuoteId == quoteId) {
-            Quote quote = optionalQuote.get();
-        try {
-            Money amountMoney = new Money.Builder()
-                .amount(quote.getTotalCharge())
-                .currency("USD")
-                .build();
+        Quote quote = (Quote) session.getAttribute(quoteSessionKey);
+        if (quote != null) {
+            model.addAttribute("quote", quote);
+        }
+            try {
+                Money amountMoney = new Money.Builder()
+                        .amount(quote.getTotalCharge())
+                        .currency("USD")
+                        .build();
 
-            // TODO: use `Address.Builder()` and the fields in `paymentFormDTO` to create an address object,
-            //  do this for shipping and billing address
-          Address billingAddress = new Address.Builder()
-                .addressLine1(paymentFormDTO.getBillingAddressLine1())
-                .addressLine2(paymentFormDTO.getBillingAddressLine2())
-                .locality(paymentFormDTO.getBillingLocality())
-                .administrativeDistrictLevel1(paymentFormDTO.getBillingAdministrativeDistrictLevel1())
-                .postalCode(paymentFormDTO.getBillingPostalCode())
-                .build();
+                Address billingAddress = new Address.Builder()
+                        .addressLine1(paymentFormDTO.getBillingAddressLine1())
+                        .addressLine2(paymentFormDTO.getBillingAddressLine2())
+                        .locality(paymentFormDTO.getBillingLocality())
+                        .administrativeDistrictLevel1(paymentFormDTO.getBillingAdministrativeDistrictLevel1())
+                        .postalCode(paymentFormDTO.getBillingPostalCode())
+                        .build();
 
-        Address shippingAddress = new Address.Builder()
-                .addressLine1(paymentFormDTO.getShippingAddressLine1())
-                .addressLine2(paymentFormDTO.getShippingAddressLine2())
-                .locality(paymentFormDTO.getShippingLocality())
-                .administrativeDistrictLevel1(paymentFormDTO.getShippingAdministrativeDistrictLevel1())
-                .postalCode(paymentFormDTO.getShippingPostalCode())
-                .build();
+                Address shippingAddress = new Address.Builder()
+                        .addressLine1(paymentFormDTO.getShippingAddressLine1())
+                        .addressLine2(paymentFormDTO.getShippingAddressLine2())
+                        .locality(paymentFormDTO.getShippingLocality())
+                        .administrativeDistrictLevel1(paymentFormDTO.getShippingAdministrativeDistrictLevel1())
+                        .postalCode(paymentFormDTO.getShippingPostalCode())
+                        .build();
 
-            CreatePaymentRequest createPaymentRequest = new CreatePaymentRequest
-                    .Builder(paymentFormDTO.getToken(), paymentFormDTO.getIdempotencyKey())
-                    .amountMoney(amountMoney)
-                    // TODO: Use the Address objects created above and add them to the request
-                    .billingAddress(billingAddress)
-                    .shippingAddress(shippingAddress)
-                    // TODO: Decide how to get customer id and order id from database for the transaction
-                    //   these are not strictly necessary for a successful payment call to Square
-                    //.customerId("W92WH6P11H4Z77CTET0RNTGFW8")
-                    //.orderId("orderId")
-                    .build();
+                CreatePaymentRequest createPaymentRequest = new CreatePaymentRequest
+                        .Builder(paymentFormDTO.getToken(), paymentFormDTO.getIdempotencyKey())
+                        .amountMoney(amountMoney)
+                        .billingAddress(billingAddress)
+                        .shippingAddress(shippingAddress)
+                        .build();
 
-            PaymentResult paymentResult = squareWrapper.createPayment(createPaymentRequest);
-            System.out.println(paymentResult.getTitle());
-            model.addAttribute("paymentResult", paymentResult);
-            if (paymentResult.getTitle().equals("SUCCESS")) {
-                System.out.println("Payment Successful");
-            } else {
-                System.out.println("Payment Failed");
-                // Handle the failure scenario, such as displaying an error message to the user or taking corrective actions
+                PaymentResult paymentResult = squareWrapper.createPayment(createPaymentRequest);
+                System.out.println(paymentResult.getTitle());
+                model.addAttribute("paymentResult", paymentResult);
+                if (paymentResult.getTitle().equals("SUCCESS")) {
+                    System.out.println("Payment Successful");
+                } else {
+                    System.out.println("Payment Failed");
+                }
+            } catch (ApiException | IOException e) {
+                System.out.println(e);
             }
-            // Process the payment result
-        } catch (ApiException | IOException e) {
-            // Handle exceptions
-            System.out.println(e);
-        }
-        } else {
-            model.addAttribute("errors", errors);
-            return "createquote";
-        }
         return "payment-successful";
     }
 }
-
-//TODO: create a payment successful page and redirect users to it!
