fe56b2a53de71f58d1f6053ea41a4c31243311fc
“Bethany <Bethanygellington@gmail.com>
2023-06-20 19:17:35 +0800

    fixes more formatting, breaks payment

--- src/main/java/org/launchcode/qleanquotes/models/enums/CleaningOption.java
+++ src/main/java/org/launchcode/qleanquotes/models/enums/CleaningOption.java
@@ -1,8 +1,8 @@
 package org.launchcode.qleanquotes.models.enums;
 
 public enum CleaningOption {
-    AVERAGE("Average Cleaning"),
-    DEEP("Deep Cleaning");
+    AVERAGE("Maintenance Clean"),
+    DEEP("Deep Clean");
 
     private final String displayName;
 
