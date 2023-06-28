b8a4a440bbc4d1dc6171b911f518d5cd700ba8df
“Bethany <Bethanygellington@gmail.com>
2023-06-28 12:13:48 +0800

    get user profile image to displayyyy, still need to clean up awful css

--- src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
+++ src/main/java/org/launchcode/qleanquotes/controllers/ProfileController.java
@@ -5,53 +5,104 @@ import jakarta.validation.Valid;
 import org.launchcode.qleanquotes.models.Customer;
 import org.launchcode.qleanquotes.models.data.CustomerRepository;
 import org.launchcode.qleanquotes.models.dto.ProfileFormDTO;
-import org.launchcode.qleanquotes.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class ProfileController {
-@Autowired
-private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @GetMapping("/profile")
     public String displayProfilePage(Model model) {
         Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("customer", customer);
         model.addAttribute("customerName", customer.getName());
-        model.addAttribute(new ProfileFormDTO());
+//        model.addAttribute(new ProfileFormDTO());
+        model.addAttribute("profileFormDTO", new ProfileFormDTO());
         model.addAttribute("title", "profile");
         return "profile";
     }
 
 
-    @PostMapping("/profile")
-    public String savePhoneNumber(@ModelAttribute @Valid ProfileFormDTO phoneNumberForm, Errors errors, Model model) {
-        Customer existingCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("phoneNumber", phoneNumberForm.getPhoneNumber());
-        existingCustomer.setPhoneNumber(phoneNumberForm.getPhoneNumber());
+//    @PostMapping("/profile")
+//    public String savePhoneNumber(@ModelAttribute @Valid ProfileFormDTO phoneNumberForm, Errors errors, Model model) {
+//        Customer existingCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        model.addAttribute("phoneNumber", phoneNumberForm.getPhoneNumber());
+//        existingCustomer.setPhoneNumber(phoneNumberForm.getPhoneNumber());
+//
+//        model.addAttribute("street", phoneNumberForm.getStreet());
+//        existingCustomer.setStreet(phoneNumberForm.getStreet());
+//        model.addAttribute("city", phoneNumberForm.getCity());
+//        existingCustomer.setCity(phoneNumberForm.getCity());
+//        model.addAttribute("zip", phoneNumberForm.getZip());
+//        existingCustomer.setZip(phoneNumberForm.getZip());
+//
+//        if (errors.hasErrors()) {
+//            model.addAttribute("customer", existingCustomer);
+//            return "profile";
+//        }
+//        customerRepository.save(existingCustomer);
+//
+//        return "redirect:/updated-profile";
+//    }
 
-        model.addAttribute("street", phoneNumberForm.getStreet());
-        existingCustomer.setStreet(phoneNumberForm.getStreet());
-        model.addAttribute("city", phoneNumberForm.getCity());
-        existingCustomer.setCity(phoneNumberForm.getCity());
-        model.addAttribute("zip", phoneNumberForm.getZip());
-        existingCustomer.setZip(phoneNumberForm.getZip());
+    @PostMapping("/profile")
+    public String saveProfileInfo(@ModelAttribute @Valid ProfileFormDTO profileForm, Errors errors, Model model) {
+        Customer existingCustomer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("customer", existingCustomer);
 
         if (errors.hasErrors()) {
-            model.addAttribute("customer", existingCustomer);
             return "profile";
         }
+
+        MultipartFile profileImage = profileForm.getProfileImage();
+
+        if (!profileImage.isEmpty()) {
+            try {
+                byte[] imageBytes = profileImage.getBytes();
+                existingCustomer.setProfileImage(imageBytes); // Set the image bytes to the customer entity
+                // Save the image bytes or process it as required
+            } catch (IOException e) {
+                // Handle the exception
+            }
+        }
+        existingCustomer.setPhoneNumber(profileForm.getPhoneNumber());
+        existingCustomer.setStreet(profileForm.getStreet());
+        existingCustomer.setCity(profileForm.getCity());
+        existingCustomer.setZip(profileForm.getZip());
+
         customerRepository.save(existingCustomer);
 
         return "redirect:/updated-profile";
-//
     }
+
+    //
+//    @GetMapping("/updated-profile")
+//    public String displayUpdatedProfile(Model model) {
+//        Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        model.addAttribute("customer", customer);
+//        model.addAttribute("customerName", customer.getName());
+//        model.addAttribute("phoneNumber", customer.getPhoneNumber());
+//        model.addAttribute(new ProfileFormDTO());
+//        model.addAttribute("title", "profile");
+//
+//        return "updated-profile";
+//    }
 
     @GetMapping("/updated-profile")
     public String displayUpdatedProfile(Model model) {
@@ -59,11 +110,23 @@ private CustomerRepository customerRepository;
         model.addAttribute("customer", customer);
         model.addAttribute("customerName", customer.getName());
         model.addAttribute("phoneNumber", customer.getPhoneNumber());
-        model.addAttribute(new ProfileFormDTO());
+        model.addAttribute("profileForm", new ProfileFormDTO()); // Updated line
         model.addAttribute("title", "profile");
 
         return "updated-profile";
     }
+
+    @GetMapping("/profile-image/{customerId}")
+    public ResponseEntity<byte[]> displayProfileImage(@PathVariable("customerId") int customerId) {
+        Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (customer != null && customer.getProfileImage() != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            return new ResponseEntity<>(customer.getProfileImage(), headers, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 
 }
 
--- src/main/java/org/launchcode/qleanquotes/models/dto/ProfileFormDTO.java
+++ src/main/java/org/launchcode/qleanquotes/models/dto/ProfileFormDTO.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+//profile image import
+import org.springframework.web.multipart.MultipartFile;
+
 public class ProfileFormDTO {
 
     @Pattern(regexp = "^(?:\\d{3}-?){1,2}\\d{4}$", message = "Phone number must be numeric and may include dashes, Ex: 123-123-1234")
@@ -20,6 +23,9 @@ public class ProfileFormDTO {
     @Pattern(regexp = "^[0-9]{5}(?:-[0-9]{4})?$", message = "Zip code should be numeric and be wither 5 digit (ex. 12345) or 9 digit (ex. 12345-6789")
     @Size(min = 5, max = 10, message="Zip code should be between 5 and 10 characters")
     private String zip;
+
+//    @Size(max = 1048576, message = "Profile image size should not exceed 1MB")
+    private MultipartFile profileImage;
 
     public String getPhoneNumber() {
         return phoneNumber;
@@ -51,5 +57,15 @@ public class ProfileFormDTO {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+
+//    getters and setters for profile image
+    public MultipartFile getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(MultipartFile profileImage) {
+        this.profileImage = profileImage;
     }
 }
